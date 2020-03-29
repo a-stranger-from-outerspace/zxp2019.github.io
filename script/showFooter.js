e93da7c96263e749fbe0e591229af261ced5ba80
zxp2019 <62792133+zxp2019@users.noreply.github.com>
2020-03-29 18:22:30 +0800

    Add files via upload

---
+++ script/showFooter.js
@@ -0,0 +1,5 @@
+var version = "3.1.3"
+var years = "2020"
+var date = "Mar 28th,2020"
+document.write('<p style="font-family:Consolas;color:white;">OIstreamblog ' + version + ' | © ' + years + ' OIstream | ' + date + ' | <a href="malito:zxps863@163.com" style="color:white;">联系我们</a>（zxps863@163.com）</p>')
+/* | <a href="https://zxp.github.io/new.txt" style="color:white;">更新记录</a> */
---
+++ script/showHeader.js
@@ -0,0 +1,3 @@
+var sChn = "每一行代码都是改变世界的一份能量"
+var sEng = "Code Changes World!"
+document.write('<div class="sentence-chn"><strong>' + sChn + '</strong></div><div class="line"><div class="S">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</div><div>·</div><div class="S">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</div></div><div class="sentence-eng"><strong>' + sEng + '</strong></div>')
