dc5cd4934efee617f7879e10633788e7c28b9258
zxp2019 <62792133+zxp2019@users.noreply.github.com>
2020-03-31 11:33:27 +0800

    Update showHeader.js

--- script/showHeader.js
+++ script/showHeader.js
@@ -1,3 +1,3 @@
-var sChn = "每一行代码都是改变世界的一份能量"
-var sEng = "Code Changes World!"
+var sChn = "愚人节快乐！"
+var sEng = "Happy April Fools' Day!"
 document.write('<div class="sentence-chn"><strong>' + sChn + '</strong></div><div class="line"><div class="S">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</div><div>·</div><div class="S">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</div></div><div class="sentence-eng"><strong>' + sEng + '</strong></div>')
