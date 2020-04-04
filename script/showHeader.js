17c56f5e2ef435eb7c52726dec24d05263b23bef
zxp2019 <62792133+zxp2019@users.noreply.github.com>
2020-04-04 14:49:28 +0800

    Update showHeader.js

--- script/showHeader.js
+++ script/showHeader.js
@@ -1,3 +1,3 @@
-var sChn = "愚人节快乐！"
-var sEng = "Happy April Fools' Day!"
+var sChn = “致敬英雄，缅怀同胞"
+var sEng = "20200404"
 document.write('<div class="sentence-chn"><strong>' + sChn + '</strong></div><div class="line"><div class="S">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</div><div>·</div><div class="S">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</div></div><div class="sentence-eng"><strong>' + sEng + '</strong></div>')
