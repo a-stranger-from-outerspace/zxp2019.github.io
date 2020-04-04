1a03e30fac9b35094bdd0c2bc458cd2972156009
zxp2019 <62792133+zxp2019@users.noreply.github.com>
2020-04-04 15:05:39 +0800

    Update showHeader.js

--- script/showHeader.js
+++ script/showHeader.js
@@ -1,3 +1,3 @@
-var sChn = “致敬英雄，缅怀同胞"
+var sChn = "致敬英雄，缅怀同胞"
 var sEng = "20200404"
 document.write('<div class="sentence-chn"><strong>' + sChn + '</strong></div><div class="line"><div class="S">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</div><div>·</div><div class="S">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</div></div><div class="sentence-eng"><strong>' + sEng + '</strong></div>')
