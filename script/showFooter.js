ceb6e80096fd8207328cdbace4419e118186140b
zxp2019 <62792133+zxp2019@users.noreply.github.com>
2020-04-16 10:44:01 +0800

    版本更新

--- script/showFooter.js
+++ script/showFooter.js
@@ -1,5 +1,5 @@
-var version = "3.1.7"
+var version = "3.1.10"
 var years = "2020"
-var date = "Apr 13th,2020"
+var date = "Apr 16th,2020"
 document.write('<p style="font-family:Consolas;color:white;">OIstreamblog ' + version + ' | © ' + years + ' OIstream | ' + date + ' | <a href="malito:zxp1863@163.com" style="color:white;">联系我们</a>（zxp1863@163.com）</p>')
 /* | <a href="https://zxp.github.io/new.txt" style="color:white;">更新记录</a> */
