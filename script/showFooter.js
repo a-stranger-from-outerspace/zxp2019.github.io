91028817a70d1afb9406b4bdebee0abc570bd215
zxp2019 <62792133+zxp2019@users.noreply.github.com>
2020-04-22 14:26:23 +0800

    Update footer for v3.1.12

--- script/showFooter.js
+++ script/showFooter.js
@@ -1,5 +1,5 @@
-var version = "3.1.11"
+var version = "3.1.12"
 var years = "2020"
-var date = "Apr 21st,2020"
+var date = "Apr 22nd,2020"
 document.write('<p style="font-family:Consolas;color:white;">OIstreamblog ' + version + ' | © ' + years + ' OIstream | ' + date + ' | <a href="malito:zxp1863@163.com" style="color:white;">联系我们</a>（zxp1863@163.com）</p>')
 /* | <a href="https://zxp.github.io/new.txt" style="color:white;">更新记录</a> */
