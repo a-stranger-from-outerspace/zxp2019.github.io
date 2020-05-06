bfe2ab8ffae84e0be9c44da4aa23a8f655ecdaab
oistream <62792133+zxp2019@users.noreply.github.com>
2020-05-06 14:42:10 +0800

    Add link for new.txt

--- script/showFooter.js
+++ script/showFooter.js
@@ -1,5 +1,5 @@
-var version = "4.1.0"
+var version = "4.1.1"
 var years = "2020"
 var date = "May 6th,2020"
-document.write('<p style="font-family:Consolas;color:white;">OIstreamblog ' + version + ' | © ' + years + ' OIstream | ' + date + ' | <a href="malito:zxp1863@163.com" style="color:white;">联系我们</a>（zxp1863@163.com）</p>')
-/* | <a href="https://zxp.github.io/new.txt" style="color:white;">更新记录</a> */
+document.write('<p style="font-family:Consolas;color:white;">OIstreamblog ' + version + ' | © ' + years + ' OIstream | ' + date + ' | <a href="malito:zxp1863@163.com" style="color:white;">联系我们</a>（zxp1863@163.com）' + ' | <a href="https://zxp2019.github.io/new.txt" style="color:white;">更新记录</a></p>')
+/* */
