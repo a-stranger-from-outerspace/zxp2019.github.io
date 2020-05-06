06fb8b911f3776245bc6eddc61629f5db664e7c1
oistream <62792133+zxp2019@users.noreply.github.com>
2020-05-06 12:09:21 +0800

    Update footer for v4.1.0

--- script/showFooter.js
+++ script/showFooter.js
@@ -1,4 +1,4 @@
-var version = "4.0.0"
+var version = "4.1.0"
 var years = "2020"
 var date = "May 6th,2020"
 document.write('<p style="font-family:Consolas;color:white;">OIstreamblog ' + version + ' | © ' + years + ' OIstream | ' + date + ' | <a href="malito:zxp1863@163.com" style="color:white;">联系我们</a>（zxp1863@163.com）</p>')
