9fddc9038ff5213f805d62919f3d3cb3f2799f07
a-stranger-from-outerspace <65579702+a-stranger-from-outerspace@users.noreply.github.com>
2020-05-25 08:23:50 +0800

    Update showFooter.js

--- script/showFooter.js
+++ script/showFooter.js
@@ -1,4 +1,4 @@
-var version = "4.5.3"
+var version = "4.5.3.1"
 var years = "2020"
-var date = "May 23rd,2020"
+var date = "May 24rd,2020"
 document.write('<p style="font-family:Consolas;color:white;">OIstreamblog ' + version + ' | © ' + years + ' OIstream | 更新于 ' + date + ' | <a href="malito:zxp1863@163.com" style="color:white;">联系我们</a>（zxp1863@163.com）' + ' | <a href="https://zxp2019.github.io/new.txt" style="color:white;">更新记录</a></p>')
