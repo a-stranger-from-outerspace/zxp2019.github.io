eedbb3fde15b753261aa109380ddea1fd714460d
oistream <62792133+zxp2019@users.noreply.github.com>
2020-07-07 14:58:43 +0800

    Update showFooter.js

--- script/showFooter.js
+++ script/showFooter.js
@@ -1,4 +1,4 @@
-var version = "4.6.2"
+var version = "4.6.3"
 var years = "2020"
-var date = "Jun 13th,2020"
+var date = "Jul 7th,2020"
 document.write('<p style="font-family:Consolas;color:white;">OIstreamblog ' + version + ' | Designed by (©) ' + years + ' OIstream & ASFOS | 更新于 ' + date + ' | <a href="malito:zxp1863@163.com" style="color:white;">联系我们</a>（zxp1863@163.com）' + ' | <a href="https://zxp2019.github.io/new.txt" style="color:white;">更新记录</a></p>')
